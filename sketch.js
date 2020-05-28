ab51b8f2b4b6579ddb63c31c9e065f88cfda41b9
Aesir-Glitch <65230467+Aesir-Glitch@users.noreply.github.com>
2020-05-28 14:04:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,6 @@ function setup() {
   createCanvas(1600,400);
   bullet = createSprite(50, 200, 10, 50);
   bullet.velocityX = speed;
-  bullet.shapeColor("white");
   wall = createSprite(1200,200,thickness,height/2);
   wall.shapeColor(80,80,80);
 
